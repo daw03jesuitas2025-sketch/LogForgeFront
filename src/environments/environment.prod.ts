1e390765b1197ee3cb1e1630c9e5e98a28670da2
Aliss <daw03.jesuitas.2025@gmail.com>
2026-04-28 14:08:25 +0200

    feat: updated the endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-apiUrl: "https://logforgeback.up.railway.app/api"
+apiUrl: "https://logforgeback.up.railway.app/api" // Sin el :8080
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "http://${environment.apiUrl/api"
+  apiUrl: "https://logforgeback.up.railway.app/api"
 };
