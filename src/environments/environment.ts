8e496348cadd69fd6492542c42ac05f1374b2990
Aliss <daw03.jesuitas.2025@gmail.com>
2026-04-29 10:15:28 +0200

    corregir errores

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "https://logforgeback.up.railway.app/api"
+  // apiUrl: "https://logforgeback.up.railway.app/api"
+    apiUrl: "logforgeback.up.railway.app"
+
 };
